872593438f95ec95e42272cd3db02093d69cfdbe
Lucas <lucasdev2124@gmail.com>
2024-04-28 03:30:06 -0300

    ready for production

--- src/utils/arrays/backendArray.ts
+++ src/utils/arrays/backendArray.ts
@@ -1,9 +1,9 @@
-import laravelImg from '/imgs/backend/laravel.png';
-import mongodbImg from '/imgs/backend/mongodb.png';
-import mysqlImg from '/imgs/backend/mysql.png';
-import nodeImg from '/imgs/backend/node.png';
-import postgresqlImg from '/imgs/backend/postgresql.png';
-import prismaImg from '/imgs/backend/prisma.png';
+import laravelImg from '../../imgs/backend/laravel.png';
+import mongodbImg from '../../imgs/backend/mongodb.png';
+import mysqlImg from '../../imgs/backend/mysql.png';
+import nodeImg from '../../imgs/backend/node.png';
+import postgresqlImg from '../../imgs/backend/postgresql.png';
+import prismaImg from '../../imgs/backend/prisma.png';
 
 const backendArray = [
     {
